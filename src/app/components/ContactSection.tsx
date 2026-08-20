c501f5f3dd1833ce09e9f556ae668fbd5b893fc5
Daniyal2513 <daniyal.shahid604@gmail.com>
2026-08-20 19:02:07 +0500

    size change

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -22,7 +22,7 @@ function BarkWidget() {
       data-image="medium-navy"
       data-version="3.0"
     >
-  <img src="/images/bark icon.png" alt="Bark" className="w-15 h-15" />
+  <img src="/images/bark icon.png" alt="Bark" className="w-20 h-20" />
 
     </a>
   );
